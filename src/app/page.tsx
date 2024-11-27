d52513b6e84b2b1fc7193ef0015e05391986324b
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-11-27 10:42:59 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,18 @@ async function HomePage() {
   const libros = await getLibros();
   return (
     <>
-      <div>
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content flex-col text-neutral-content text-center">
-          <div className="flex justify-between m-10">
-            <h1 className="text-4xl font-bold text-slate-200">Audiobook</h1>
-          </div>
-          <p className="text-violet-200">Registra y guarda las suculentas de tu colección</p>
-        </div>
-      </div>
+      <div className="">
       <div className="container my-10">
         <div className="mb-10">
-          <Link href="/libros/new" className={buttonVariants()}>
-            Registrar libro
-          </Link>
+          <Button>Registrar libro</Button>
         </div>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
           {libros.map((libro: any) => (
             <LibroCard key={libro.id} libro={libro} />
           ))}
         </div>
       </div>
+    </div>
     </>
   );
 }
