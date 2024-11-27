61242e846526b0b7b951de5e2048066a9f9080dd
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-11-27 14:02:31 -0300

    Update LibroCard.tsx

--- src/components/LibroCard.tsx
+++ src/components/LibroCard.tsx
@@ -13,19 +13,28 @@ export function LibroCard({ libro }: any) {
   }
 
   return (
-    <Card className="bg-slate-800 border-slate-200 text-slate-300">
-      <div
-        className="w-full h-72  bg-contain bg-no-repeat bg-center bg-slate-950 rounded-t-lg"
-        style={{ backgroundImage: `url(${libro.portada})` }}
-      ></div>
-      <CardContent className="p-6  rounded-b-lg">
-        <CardTitle className="text-slate-300">{libro.titulo}</CardTitle>
-        <div>{libro.autor}</div>
-
-        <CardFooter className="flex justify-between p-0">
-          <span className="flex"><img src="/star.svg" className="w-4 mr-2" alt="star" />{libro.puntuacion}</span>
-        </CardFooter>
-      </CardContent>
+    <Card className="libro-card h-72 bg-slate-800 border-slate-200 text-slate-300 cursor-pointer rounded-lg">
+      <div className="front w-full absolute">
+        <div
+          className="w-full h-72  bg-contain bg-no-repeat bg-center bg-slate-950 rounded-lg"
+          style={{ backgroundImage: `url(${libro.portada})` }}
+        ></div>
+      </div>
+      <div className="back w-full h-full absolute">
+        <CardContent className="p-6  rounded-b-lg">
+          <CardTitle className="text-2xl font-bold mb-4">
+            {libro.titulo}
+          </CardTitle>
+          <div className="text-lg font-thin">{libro.autor}</div>
+          <CardFooter className="flex justify-between p-0">
+            <div className="text-sm font-bold">{libro.categoria}</div>
+            <span className="flex">
+              <img src="/star.svg" className="w-4 mr-2" alt="star" />
+              {libro.puntuacion}
+            </span>
+          </CardFooter>
+        </CardContent>
+      </div>
     </Card>
   );
 }
